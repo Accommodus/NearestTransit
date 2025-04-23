bc393dafbd2bf12cd2ead7ed3ba1e5fcc8ca03ee
Sean-Staszewski <166570500+Sean-Staszewski@users.noreply.github.com>
2025-04-22 23:24:20 -0400

    Update build_cli.nim

--- cli/build_cli.nim
+++ cli/build_cli.nim
@@ -1,21 +1,43 @@
 include ../information/settings
-const treeData = staticRead(treeFileRelative)
-
 import strutils, os
 import ../sort/[compare_algs, quick_sort, heap_min]
 import ../process_data/construct_tree
 import ../information/types
 
-let
-    lat = parseFloat(paramStr(1))
-    lon = parseFloat(paramStr(2))
-    k = parseInt(paramStr(3))
-var
-    tree = loadTreeJsonStr[TranSeq](treeData)
-let
-    coord = newCoord(0,0)
-    sortPoint = newCoord(lat, lon)
-    durations = KNNSort(tree, coord, k, sortPoint, [quickSortEntry[DistData], heapSort[DistData]])
+when isMainModule:
+  const treeData = staticRead(treeFileRelative)
+  var tree = loadTreeJsonStr[TranSeq](treeData)
 
-echo "QuickSort took: ", durations[0][0]
-echo "HeapSort took: ", durations[0][1]
+  while true:
+    echo "\nEnter latitude (or 'exit' to quit): "
+    let latInput = readLine(stdin)
+    if latInput.strip.toLowerAscii == "exit":
+      break
+
+    echo "Enter longitude: "
+    let lonInput = readLine(stdin)
+    echo "Enter k: "
+    let kInput = readLine(stdin)
+
+    try:
+      let
+        lat = parseFloat(latInput)
+        lon = parseFloat(lonInput)
+        k = parseInt(kInput)
+
+        coord = newCoord(0, 0)
+        sortPoint = newCoord(lat, lon)
+        durations = KNNSort(tree, sortPoint, k, coord, [quickSortEntry[DistData], heapSort[DistData]])
+
+      echo "QuickSort took: ", durations[0][0]
+      echo "HeapSort took: ", durations[0][1]
+
+      echo "Closest ", k, " Stops: "
+      for disData in durations[1]:
+        echo "  Distance: ", disData.distance, "  Coords: ", disData.coord
+        for stop in disData.data:
+            echo "      Name: ", stop.stop_name
+    except ValueError:
+      echo "Invalid input. Please try again."
+
+  echo "Exited program."
